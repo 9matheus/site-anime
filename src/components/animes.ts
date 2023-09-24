ff2a9259c1a18902e6b4fdff524efdfd01429237
matheus <matheus.martins.marques12@gmail.com>
2023-09-23 23:54:03 -0300

    first commit

---
+++ src/components/animes.ts
@@ -0,0 +1,30 @@
+export const one = [
+  "/cards/Item-01.png",
+  "/cards/Item-02.png",
+  "/cards/Item-03.png",
+  "/cards/Item-04.png",
+  "/cards/Item-05.png",
+  "/cards/Item-06.png",
+  "/cards/Item-07.png",
+  "/cards/Item-08.png",
+  "/cards/Item-09.png",
+  "/cards/Item-10.png",
+  "/cards/Item-11.png",
+  "/cards/Item-12.png",
+];
+
+export const two = [
+  "/poster/01.webp",
+  "/poster/02.jpeg",
+  "/poster/03.webp",
+  "/poster/04.webp",
+  "/poster/05.webp",
+  "/poster/06.jpeg",
+  // "/poster/cards/item-06.png",
+  // "/poster/cards/item-07.png",
+  // "/poster/cards/item-08.png",
+  // "/poster/cards/item-09.png",
+  // "/poster/cards/item-10.png",
+  // "/poster/cards/item-11.png",
+  // "/poster/cards/item-12.png",
+];
