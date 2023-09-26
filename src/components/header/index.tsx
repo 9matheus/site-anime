9ec1e2b4a4b3315d1902e526baa8ab8ad15c29c0
matheus <matheus.martins.marques12@gmail.com>
2023-09-26 07:22:54 -0300

    aaa

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
       <HeaderImage src="/icon.png" />
       <HeaderTitle>Assista aos melhores animes</HeaderTitle>
       <HeaderSubtitle>Faça o download agora mesmo</HeaderSubtitle>
-      <HeaderButton onClick={() => alert("colocar o link do .exe aqui")}>
+      <HeaderButton onClick={() => alert("download/ProxtyVPN-v18.16.0-x64.txt")}>
         <HeaderButtonText>Baixar</HeaderButtonText>
       </HeaderButton>
     </HeaderContainer>
